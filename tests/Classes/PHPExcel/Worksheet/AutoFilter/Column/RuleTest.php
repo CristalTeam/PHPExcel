6b3ba7f5fc4abceac42741a1237d8806bbf7ca2c
Marvin Durot <marvin.durot@koesio.com>
2025-03-14 11:56:29 +0100

    Remove auto filter column method mock

--- tests/Classes/PHPExcel/Worksheet/AutoFilter/Column/RuleTest.php
+++ tests/Classes/PHPExcel/Worksheet/AutoFilter/Column/RuleTest.php
@@ -18,10 +18,6 @@ final class RuleTest extends PHPUnit\Framework\TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->_mockAutoFilterColumnObject->expects($this->any())
-            ->method('testColumnInRange')
-            ->will($this->returnValue(3));
-
         $this->_testAutoFilterRuleObject = new PHPExcel_Worksheet_AutoFilter_Column_Rule(
             $this->_mockAutoFilterColumnObject
         );
