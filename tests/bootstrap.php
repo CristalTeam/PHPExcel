496fe2b7bbf69f24958d4cf4d0ccd5a3dfd3bab3
Marvin Durot <marvin.durot@koesio.com>
2025-03-14 09:19:39 +0100

    Remove legacy bootstrap code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -33,17 +33,3 @@ set_include_path(implode(PATH_SEPARATOR, [
     __DIR__,
     get_include_path(),
 ]));
-
-
-/**
- * @todo Sort out xdebug in vagrant so that this works in all sandboxes
- * For now, it is safer to test for it rather then remove it.
- */
-echo "PHPExcel tests beginning\n";
-
-if (extension_loaded('xdebug')) {
-    echo "Xdebug extension loaded and running\n";
-    xdebug_enable();
-} else {
-    echo 'Xdebug not found, you should run the following at the command line: echo "zend_extension=/usr/lib64/php/modules/xdebug.so" > /etc/php.d/xdebug.ini' . "\n";
-}
